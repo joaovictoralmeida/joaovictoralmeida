157eca5112f950f4949798a87db2911e91adb3ed
joaovictoralmeida <124613767+joaovictoralmeida@users.noreply.github.com>
2025-01-21 19:09:46 -0300

    ...

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return(
-        <div className="flex flex-col items-center w-screen bg-[#161616]">
+        <div className="flex flex-col items-center bg-[#161616]">
             <div className="flex justify-center w-[1216px] p-5">
                 <h1 className="text-white font-inter">&copy;2025 - João Victor Almeida</h1>
             </div>
